2773b852e4eebe34a1224b7763d9901c8480ddb0
Hector Sanchez <h.manuel2311@hotmail.com>
2022-04-27 11:47:20 -0600

    first commit

---
+++ backend/products/serializers.py
@@ -0,0 +1,53 @@
+from wsgiref.validate import validator
+from rest_framework import serializers
+from rest_framework.reverse import reverse
+
+from .models import Product
+from .validators import validate_title
+
+class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk'
+    )
+    title = serializers.CharField(validators=[validate_title])
+    class Meta:
+        model = Product
+        fields = [
+            'url',
+            'edit_url',
+            'pk',
+            'title',
+            'content',
+            'price',
+            'sale_price',
+            'my_discount',
+        ]
+
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
+
+    # def create(self, validated_data):
+    #     obj = super().create(validated_data)
+    #     return obj
+
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title')
+    #     return instance
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
+
+    def get_my_discount(self, obj):
+        if not hasattr(obj, 'id'):
+            return None
+        if not isinstance(obj, Product):
+            return None
