ee1b63deb0df756367a35f20eafa53e65f14986d
Hector Sanchez <h.manuel2311@hotmail.com>
2022-06-16 23:30:50 -0600

    qty select add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
       <main className="py-3">
         <Container>
           <Routes>
-            {" "}
             <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
+            <Route path='/cart/:prodId' element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
