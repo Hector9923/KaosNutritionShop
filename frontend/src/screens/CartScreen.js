d7e42524f4dc10794bfd6201c9672b8205e57054
Hector Sanchez <h.manuel2311@hotmail.com>
2022-06-18 22:11:52 -0600

    add to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Message } from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-function CartScreen() {
+function CartScreen({ location, useNavigate }) {
+  const productId = productId.id
+  const qty = localtion.search ? numberLocation.search.split('=')[1] : 1
+  console.log('qty', qty)
+
   return (
     <div>Cart</div>
   )
