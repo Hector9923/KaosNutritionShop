f6e867da830ae38d145a0b4e02319d2527abedfe
Hector Sanchez <h.manuel2311@hotmail.com>
2022-06-23 23:08:59 -0600

    fixed add to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,16 @@
 import React, {useEffect} from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import { Message } from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
-function CartScreen({ location, useNavigate }) {
-  const productId = productId.id
-  const qty = localtion.search ? numberLocation.search.split('=')[1] : 1
-  console.log('qty', qty)
+function CartScreen({}) {
+  const location = useLocation()
+  // const {id} = useParams()
+  // const productId = id
+  const qty = location.search ? location.search.split('=') : 1
+  console.log('qty:', qty)
 
   return (
     <div>Cart</div>
