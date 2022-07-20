89c55aa2f16d110c74d232943b835c22efd02782
Hector Sanchez <h.manuel2311@hotmail.com>
2022-07-19 21:49:52 -0600

    error 500 still broken

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from base.views import order_views as views
 
 
-urlpatterns = []
+urlpatterns = [
+    # path('', views.getOrders, name='orders'),
+    path('add/', views.addOrderItems, name='orders-add'),
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+]
